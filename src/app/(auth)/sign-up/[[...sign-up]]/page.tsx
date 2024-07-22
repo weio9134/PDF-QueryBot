23af7d912db850131135c4c549268ca6dd34ba0e
Wei Ouyang <wo52@cornell.edu>
2024-07-22 17:27:58 -0400

    setup signin and signup and landing page

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <SignUp />
+  )
+}
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default async function Home() {
-  return (
-    <main className="w-screen min-h-screen bg-gradient-to-b from-blue-100 via-blue-300 to-blue-500">
-      Hello World
-    </main>
-  );
-}
