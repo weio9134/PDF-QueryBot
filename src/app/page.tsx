4cb6255f09423db2e269a2504d245a2a67ab5e0b
Wei Ouyang <wo52@cornell.edu>
2024-07-22 16:39:29 -0400

    setup clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <main>
+    <main className="w-screen min-h-screen bg-gradient-to-b from-blue-100 via-blue-300 to-blue-500">
       Hello World
     </main>
   );
